2919bdfcb0643dd48bed55adc67038ffb5132c1d
angularsantanderi <angularsantanderi@gmail.com>
2022-02-08 13:45:03 +0100

    add topscore

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ComponenteEnLineaComponent } from './componente-en-linea/componente-en-linea.component';
+import { MarcadorComponent } from './marcador/marcador.component';
+import { EquipoComponent } from './equipo/equipo.component';
+import { TopScoreComponent } from './top-score/top-score.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ComponenteEnLineaComponent
+    ComponenteEnLineaComponent,
+    MarcadorComponent,
+    EquipoComponent,
+    TopScoreComponent
   ],
   imports: [
     BrowserModule
